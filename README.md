367978ea664e2de2759f2bdc26ff656ac3303d74
Dominik Hofmann <Dominik@Dominik-Hofmanns-MacBook-Air.local>
2011-10-01 16:12:36 -0400

    added basic conditionals, selectors on literals, comments

--- README.md
+++ README.md
@@ -1,17 +1,38 @@
 Shorthand language for iOS development, compiles to Objective-C
 
 ```coffee
-someView = UIView!!
+# variable assignment and autoreleased constructor
+someView = UIView!
+
+# selector invocation and CGRect shortcut
 someView setFrame:@(0, 0, 200, 200)
-url = 'http://google.com'
-req = NSURLRequest! withURL:(NSURL! withString:someString)
+
+# conditionals, string literals, and nested selectors
+if someCondition
+	path = 'http://google.com'
+	req = NSURLRequest! withURL:(NSURL! withString:path)
+else
+ 	uppercase = 'THIS TEXT IS CAPITALIZED'
+	lowercase = capitalizedText lowercaseString
+	hello = ('hello/world' componentsSeparatedByString:'/') objectAtIndex:2
+end
+
+# current status
+status = 'pre-pre-pre-alpha'
 ```
 
 becomes this:
 
 ```objective-c
-UIView *someView = [[UIView alloc] init];
+UIView *someView = [[[UIView alloc] init] autorelease];
 [someView setFrame:CGRectMake(0, 0, 200, 200)];
-NSString *url = @"http://google.com";
-NSURLRequest *req = [[[NSURLRequest alloc] initWithURL:[[[NSURL alloc] initWithString:someString] autorelease]] autorelease];
+if (someCondition) {
+NSString *path = @"http://google.com";
+NSURLRequest *req = [[[NSURLRequest alloc] initWithURL:[[[NSURL alloc] initWithString:path] autorelease]] autorelease];
+} else {
+NSString *uppercase = @"THIS TEXT IS CAPITALIZED";
+id lowercase = [capitalizedText lowercaseString];
+id hello = [[@"hello/world" componentsSeparatedByString:@"/"] objectAtIndex:2];
+}
+NSString *status = @"pre-pre-pre-alpha";
 ```
--- khaki.c
+++ khaki.c
@@ -1,3 +1,4 @@
+// helpers
 %{
   
   String.prototype.capitalize = function() {
@@ -5,15 +6,20 @@
   }
   
   print = function (object) {
+    console.log(expr(object));
+  }
+  
+  expr = function (object) {
+    if (object == undefined) return '';
     if (object.expr)
-      console.log(object.expr + ';');
+      return (object.expr);
     else
-      console.log(object + ';');
+      return (object);
   }
   
 %}
 
-/* lexical grammar */
+// lex
 %lex
 %%
 
@@ -25,7 +31,10 @@
 
 \s*\=\s*                            return '='
 
-//\s([a-zA-Z]+\:+.*)                return 'SELECTOR'
+"if"\s*                             return 'IF'
+"else"                              return 'ELSE'
+"end"                               return 'END'
+
 \s+([a-zA-Z]+\:{1})                 return 'SELECTOR_ARG'
 \s+([a-zA-Z]+)                      return 'SELECTOR_NOARG'
 
@@ -35,18 +44,18 @@
 [a-zA-Z][a-zA-Z0-9]*                return 'WORD'
 "+"                                 return '+'
 "!"                                 return '!'
-'@'                                 return '@'
-'('                                 return '('
-')'                                 return ')'
-'['                                 return '['
-']'                                 return ']'
+"@"                                 return '@'
+"("                                 return '('
+")"                                 return ')'
+"["                                 return '['
+"]"                                 return ']'
 ","                                 return ','
 <<EOF>>                             return 'EOF'
+[\#]+.+                             return 'COMMENT'
 .                                   return 'INVALID'
 
 /lex
-
-/* operator associations and precedence */
+// operator associations
 
 %left '+'
 %left '='
@@ -54,18 +63,26 @@
 
 %start program
 
-%% /* language grammar */
+%%
+// grammar
 
 program
     : expressions
-      { console.log("\n\nFinished compiling"); }
+    { console.log("\n" + $expressions); console.log("\nFinished compiling"); }
     ;
     
-expressions:
+expressions
+    : 
+    | expressions COMMENT NEWLINE
+      { $$ = expr($1) + ''; }
+    | expressions IF e NEWLINE expressions ELSE NEWLINE expressions END NEWLINE
+      { $$ = expr($1) + 'if (' + $3 + ") {\n" + $5 + "} else {\n" + $8 + "}\n"; }
+    | expressions IF e NEWLINE expressions END NEWLINE
+      { $$ = expr($1) + 'if (' + $3 + ") {\n" + $5 + "}\n"; }
     | expressions e NEWLINE
-      { print($e); }
+      { $$ = expr($1) + expr($2) + ";\n"; }
     | expressions e EOF
-      { print($e); }
+      { $$ = expr($1) + expr($2) + ";\n"; }
     ;
     
 e
@@ -79,16 +96,16 @@ e
       else
         if (!$e.scalar) $$ = 'id ' + $1 + ' = ' + $3;
     }}
-    | construct
-    | message
-    | shortcut
     | STRING_LITERAL
     {{
       $$ = {
         type: 'NSString',
-        expr: '@"' + yytext.substr(1).slice(0, -1) +'"'
+        expr: '@"' + yytext.substr(1).slice(0, -1) + '"'
       }
     }}
+    | construct
+    | message
+    | shortcut
     | WORD
       {$$ = yytext;}
     | NUMBER
@@ -97,10 +114,17 @@ e
 
 message
     : '(' message ')'
-      {$$ = '[' + $2 + ']';}
+      {$$ = $2;}
+    | e selector_args
+      {$$ = '[' + expr($1) + $2 + ']';}
+    | STRING_LITERAL selector_args
+      {$$ = '[@"' + $STRING_LITERAL.substr(1).slice(0, -1) + '"' + $2 + ']';}
     | WORD selector_args
       {$$ = '[' + $1 + $2 + ']';}
+    | STRING_LITERAL SELECTOR_NOARG
+      {$$ = '[@"' + $STRING_LITERAL.substr(1).slice(0, -1) + '"' + $2 + ']';}
     | WORD SELECTOR_NOARG
+      {$$ = '[' + $1 + $2 + ']';}
     ;
 
 selector_args
@@ -118,12 +142,6 @@ selector_arg
         $$ = $1 + $2;
       }
     }}
-/*
-    : SELECTOR_ARG WORD
-      {$$ = $1 + $2;}
-    | SELECTOR_ARG NUMBER
-      {$$ = $1 + $2;}
-*/
     | SELECTOR_ARG
     ;
     
